495f2649cf9d5fe097625d45f9635a049fedcdb0
Hoormazd <hoormazd@pirayeshfar.ch>
2022-05-12 20:43:28 +0200

    declutter

--- src/App.vue
+++ src/App.vue
@@ -13,35 +13,45 @@ var scoreval = 0;
 
 const questions = ref([
   {
-	question: 'What is Vue?',
-	answer: 0,
-	options: [
-		'A framework',
-		'A library',
-		'A type of hat'
-	],
-	selected: null
-  },
-  {
-	question: 'What is Vuex used for?',
-	answer: 2,
-	options: [
-		'Eating a delicious snack',
-		'Viewing things',
-		'State management'
-	],
-	selected: null
-  },
-  {
-	question: 'What is Vue Router?',
+	question: 'Test Question',
 	answer: 1,
 	options: [
-		'An ice cream maker',
-		'A routing library for Vue',
-		'Burger sauce'
+		'Wrong',
+		'Correct',
+		'Also Wrong'
 	],
 	selected: null
-  }
+  },
+//   {
+// 	question: 'What is Vue?',
+// 	answer: 0,
+// 	options: [
+// 		'A framework',
+// 		'A library',
+// 		'A type of hat'
+// 	],
+// 	selected: null
+//   },
+//   {
+// 	question: 'What is Vuex used for?',
+// 	answer: 2,
+// 	options: [
+// 		'Eating a delicious snack',
+// 		'Viewing things',
+// 		'State management'
+// 	],
+// 	selected: null
+//   },
+//   {
+// 	question: 'What is Vue Router?',
+// 	answer: 1,
+// 	options: [
+// 		'An ice cream maker',
+// 		'A routing library for Vue',
+// 		'Burger sauce'
+// 	],
+// 	selected: null
+//   }
 ])
 
 const quizCompleted = ref(false)
